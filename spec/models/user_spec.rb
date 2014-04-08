96fb53bdc1ed3903a5a9e4abb2ed653e232b96aa
ProfessionalMate <materonald3@gmail.com>
2014-04-08 12:37:05 +0300

    Finish Userauthentication suite

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -22,6 +22,8 @@ describe User do
   it {should respond_to(:password_digest)}
   it {should respond_to(:password)}
   it {should respond_to(:password_confirmation)}
+  it {should respond_to(:remember_token)}
+  it {should respond_to(:authenticate)}
 
   it {should be_valid}
 
@@ -54,7 +56,6 @@ describe User do
 		@user.reload.email.should == mixed_case_email.downcase
 		end
 	end
-
 end
 	describe "when user email format is valid" do
 		it "should be valid" do
@@ -102,5 +103,9 @@ end
 			before {@user.password = @user.password_confirmation = "a"*5}
 			it {should  be_invalid}
 		end
+	end
+	describe "remember token" do
+		before {@user.save}
+		its(:remember_token) {should_not be_blank}
 	end
 end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -29,6 +29,14 @@ describe "profile page" do
 			end
 			it "should create a user" do
 			expect {click_button submit}.to change(User, :count).by(1)
+		end
+			describe "after saving the user" do
+				before {click_button submit}
+				let(:user) {user.find_by_email('user@example.com')}
+
+				it {should have_selector('title', text: user.name)}
+				it {should have_selector('div.alert.alert-success', text: 'You are now registered on Instruction App')}
+				it {should have_link('Sign out')}
 			end
 		end
 	end
